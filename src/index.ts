86c4cb9af4dd057731601a5d8290ef11c5372d41
Vladimir Gutorov <vlgutv@gmail.com>
2018-08-02 00:03:55 +0300

    Setup ENV config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import * as Discord from 'discord.js';
-const client = new Discord.Client();
 
-client.login('token');
+const config = require('./common/env/config');
+const client = new Discord.Client();
+const GUILD_NAME = process.env.GUILD_NAME || config.ENV.GUILD_NAME;
+const CHANNELS = process.env.CHANNELS || config.ENV.CHANNELS;
+const BOT_TOKEN = process.env.BOT_TOKEN || config.ENV.BOT_TOKEN;
+const APP_SLUG = process.env.APP_SLUG || config.ENV.APP_SLUG;
+const PORT = process.env.PORT || config.ENV.PORT;
+const DEBUG = process.env.DEBUG || config.ENV.DEBUG;
+
+client.login(BOT_TOKEN);
