3732702832de46ca07016163914f21348d9278cc
Vladimir Gutorov <vlgutv@gmail.com>
2018-08-04 17:01:52 +0300

    Setup TG bot

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import * as Discord from 'discord.js';
 import * as discordService from './common/services/discord.service';
 import generateWelcomeMessage from './common/services/messages.service';
-
+const Telegraf = require('telegraf');
 const config = require('./common/env/config');
-const client = new Discord.Client();
+
 const CHANNEL = process.env.CHANNEL || config.ENV.CHANNEL;
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN || config.ENV.DISCORD_BOT_TOKEN;
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || config.ENV.TELEGRAM_BOT_TOKEN;
 
-client.login(DISCORD_BOT_TOKEN);
+const discordBot = new Discord.Client();
+const TGBot = new Telegraf(TELEGRAM_BOT_TOKEN);
 
-client.on('message', message => {
+discordBot.login(DISCORD_BOT_TOKEN);
+console.log(TGBot);
+
+discordBot.on('message', message => {
   let channel: any = message.channel as any;
   let isAllowedChannel: boolean = discordService.isWelcomeChannel(channel.name, CHANNEL);
   let isWelcomeMessage: boolean = discordService.isWelcomeMessage(message.type);
