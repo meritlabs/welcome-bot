aa69028b995543c11230459ea027cb1c5964aa09
Vladimir Gutorov <vlgutv@gmail.com>
2018-08-16 23:41:06 +0300

    Update content

--- src/common/services/messages.service.ts
+++ src/common/services/messages.service.ts
@@ -7,12 +7,13 @@ and validate it on the Merit blockchain before you can send, receive, or mine MR
 Invites are distributed among the community via our mining algorithm. 
 The easiest way to get invites to share with your friends is to ask the person who invited you.  
 To check who that is go to: http://blockstats.merit.me/wallet and enter your address or alias.
-\n2. You should read our FAQ! https://www.merit.me/faq
-\n3. If you are interested in mining: we have CPU mining and GPU mining for NVIDIA in 
+\n2. Don't have a wallet? https://www.merit.me/blog/setting-up-your-first-merit-wallet/
+\n3. You should read our FAQ! https://www.merit.me/faq
+\n4. If you are interested in mining: we have CPU mining and GPU mining for NVIDIA in 
 the lightwallet and are working on additional mining resources.
-\n4. One-click mining is available in both the desktop lightwallet and core wallet.
+\n5. One-click mining is available in both the desktop lightwallet and core wallet.
 You cannot mine from the web wallet. Get the wallets here: https://www.merit.me/get-started/
-\n5. You can trade BTC for MRT on the Merit Market https://market.merit.me/dashboard
-\n6. If you have questions, let us know!`;
+\n6. You can trade BTC for MRT on the Merit Market https://market.merit.me/dashboard
+\n7. If you have questions, let us know!`;
   return message;
 }
